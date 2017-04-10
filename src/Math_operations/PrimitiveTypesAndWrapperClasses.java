aebfb3ce4f8841369a673dd60ec979c836a489e3
kirillk <kirill.krasnobaiev@gmail.com>
2017-04-10 14:36:25 +0300

    Polimorfizm2

--- src/Math_operations/PrimitiveTypesAndWrapperClasses.java
+++ src/Math_operations/PrimitiveTypesAndWrapperClasses.java
@@ -3,14 +3,15 @@ package Math_operations;
 /**
  * Created by kirillk on 10.04.17.
  */
-public class PrimitiveTypesAndWrapperClasses extends Main {
+public class PrimitiveTypesAndWrapperClasses {
     public static void PrimitiveTypes(){
         //Autoboxing, unboxing
-        Integer b = 256;
+        Integer b = 100;
         long c = b;
         int d = (int) c;
         double e = d;
         byte a = (byte) e;
-        System.out.println(a);
+        int i = b;
+        System.out.println(i);
     }
 }
